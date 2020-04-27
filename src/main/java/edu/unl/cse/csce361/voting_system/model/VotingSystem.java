66d74bebba0ecb763042f85d1c17f0d01b51377d
jimmy.erickson <jimmy.erickson@huskers.unl.edu>
2020-04-27 15:34:48 -0500

    added candidate management methods in votingsystem.java

--- src/main/java/edu/unl/cse/csce361/voting_system/model/VotingSystem.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/model/VotingSystem.java
@@ -1,47 +1,55 @@
 package edu.unl.cse.csce361.voting_system.model;
 
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
 public class VotingSystem {
 	/*
-	 * Methods (JDBC) to communicate with
-	 * SQL database
+	 * Methods (JDBC) to communicate with SQL database
 	 */
-	
+
 	public static void addCandidate(Candidate candidate) throws SQLException {
-		String values = String.format("VALUES(%s,%s,%s,%s,%i)", candidate.getFirstName(), candidate.getLastName(), candidate.getParty(), candidate.getPosition(), candidate.getVouteCount());
+		String values = String.format("VALUES(%s,%s,%s,%s,%i)", candidate.getFirstName(), candidate.getLastName(),
+				candidate.getParty(), candidate.getPosition(), candidate.getVouteCount());
 		String query = "INSERT INTO Candidates(firstName, lastName, party, position, voteCount) " + values;
 		Connection connection = null;
 		Statement statement = null;
-		try {
-			connection = Database.getConnection();
-			statement = connection.createStatement();
-			statement.executeQuery(query);
-		} finally {
-			statement.close();
-			connection.close();
+		if (validateCandidate(candidate) == false) {
+			try {
+				connection = Database.getConnection();
+				statement = connection.createStatement();
+				statement.executeQuery(query);
+			} finally {
+				statement.close();
+				connection.close();
+			}
 		}
 	}
-	
-	public static boolean validateCandidate(String firstName, String lastName, String party, String position) {
-		//TODO: Verify that proposed candidate has not already been created by querying their name
-		//Returns true if candidate has not been created and false if they already have
-		return true;
+
+	public static boolean validateCandidate(Candidate candidate) throws SQLException {
+		ArrayList<Candidate> candidates = getCandidates();
+		if (candidates.contains(candidate)) {
+			return false;
+		} else {
+			return true;
+		}
 	}
-	
-	public static Candidate getCandidate(String firstName, String lastName) {
-		//TODO: Query the database for candidate, create object, and return it
-		Candidate candidate = null;
-		return candidate;
+
+	public static Candidate getCandidate(String firstName, String lastName) throws SQLException {
+		ArrayList<Candidate> candidates = getCandidates();
+
+		for (Candidate candidate : candidates) {
+			if (candidate.getFirstName().equals(firstName) && candidate.getLastName().equals(lastName)) {
+				return candidate;
+			}
+		}
+		return null;
 	}
-	
-	public static ArrayList<Candidate> getCandidates() throws SQLException{
+
+	public static ArrayList<Candidate> getCandidates() throws SQLException {
 		String query = "SELECT * FROM Candidates";
 		ArrayList<Candidate> candidates = new ArrayList<Candidate>();
 		Candidate candidate = null;
@@ -52,10 +60,11 @@ public class VotingSystem {
 			connection = Database.getConnection();
 			statement = connection.createStatement();
 			rs = statement.executeQuery(query);
-			while(rs.next()) {
-				candidate = new Candidate(rs.getString("firstName"), rs.getString("lastName"), rs.getString("party"), rs.getString("position"), rs.getInt("voteCount"));
+			while (rs.next()) {
+				candidate = new Candidate(rs.getString("firstName"), rs.getString("lastName"), rs.getString("party"),
+						rs.getString("position"), rs.getInt("voteCount"));
 				candidates.add(candidate);
-				
+
 			}
 		} finally {
 			rs.close();
@@ -64,42 +73,56 @@ public class VotingSystem {
 		}
 		return candidates;
 	}
-	
-	public static void removeCandidate(Candidate candidate) {
-		//TODO: Query database to remove candidate
+
+	public static void removeCandidate(Candidate candidate) throws SQLException {
+		String query = String.format("DELETE FROM Candidates WHERE firstName='%s' AND lastName='%s'",candidate.getFirstName(),candidate.getLastName());
+		Connection connection = null;
+		Statement statement = null;
+		if (validateCandidate(candidate) == true) {
+			try {
+				connection = Database.getConnection();
+				statement = connection.createStatement();
+				statement.executeQuery(query);
+			} finally {
+				statement.close();
+				connection.close();
+			}
+		}
 	}
-	
+
 	public static String getVoterInfo(String firstName, String lastName) {
-		//TODO: Query the database to get voter's first/last name, and whether or not they have voted.
-		//Return well formatted String with voter info
-		//Return "Invalid Input, voter does not exist" if voter is not in database
+		// TODO: Query the database to get voter's first/last name, and whether or not
+		// they have voted.
+		// Return well formatted String with voter info
+		// Return "Invalid Input, voter does not exist" if voter is not in database
 		return "";
 	}
-	
+
 	public static String getAllVoterInfo() {
-		//TODO: Query Voter table to get all accessible voter info (name/wheter they have voted)
-		//Return well formatted String with all voter info
-		//Return "No voter information available" if table is empty
+		// TODO: Query Voter table to get all accessible voter info (name/wheter they
+		// have voted)
+		// Return well formatted String with all voter info
+		// Return "No voter information available" if table is empty
 		return "";
 	}
-	
+
 	public static void addProposition(Proposition proposition) {
-		//TODO: Query database to add proposition to proposition table
+		// TODO: Query database to add proposition to proposition table
 	}
-	
+
 	public static boolean validateProposition(Proposition proposition) {
-		//TODO: Verify that proposition has not already been added to prop table
-		//Returns true if proposition has not been added, false if it already has
+		// TODO: Verify that proposition has not already been added to prop table
+		// Returns true if proposition has not been added, false if it already has
 		return true;
 	}
-	
+
 	public static ArrayList<Proposition> getPropositions() {
-		//TODO: Query database to return an ArrayList of all propositions
+		// TODO: Query database to return an ArrayList of all propositions
 		ArrayList<Proposition> propositions = new ArrayList<Proposition>();
 		return propositions;
 	}
-	
+
 	public static void removeProposition(Proposition proposition) {
-		//TODO: Query database to remove proposition from table
+		// TODO: Query database to remove proposition from table
 	}
 }
